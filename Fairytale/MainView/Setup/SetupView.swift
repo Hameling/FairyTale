cc4dc075c539bc79bd86dcb21a292d50fa0bb577
백승한 <hameli@naver.com>
2018-12-04 10:20:36 +0900

    앱 등록전 커밋

---
+++ Fairytale/MainView/Setup/SetupView.swift
@@ -0,0 +1,30 @@
+//
+//  SetupView.swift
+//  Fairytale
+//
+//  Created by 백승한 on 21/11/2018.
+//  Copyright © 2018 WunGim. All rights reserved.
+//
+
+import UIKit
+
+class SetupView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fairytale/StringExtension.swift
@@ -0,0 +1,9 @@
+//
+//  StringExtension.swift
+//  Fairytale
+//
+//  Created by 백승한 on 26/11/2018.
+//  Copyright © 2018 WunGim. All rights reserved.
+//
+
+import Foundation
